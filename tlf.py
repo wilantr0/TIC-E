78b5bbe9280eb810d4a9119aaae2b20380f2b101
wilantr0 <guilleballarin73@gmail.com>
2023-10-05 16:19:52 +0200

    innecesario

--- tlf.py
+++
@@ -1,8 +0,0 @@
-import phonenumbers
-from phonenumbers import geocoder
-
-
-phonenumbers_1 = phonenumbers.parse('+34691155874')
-geo = geocoder.description_for_number(phonenumbers_1, 'en')
-
-print(geo)
