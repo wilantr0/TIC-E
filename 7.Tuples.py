50cedf6793a4d4709d1a8c809e552fb9c4fc7749
Guillermo <guilleballarin73@gmail.com>
2023-10-19 13:26:05 +0200

    Tuples and Dicts Creation

---
+++ 7.Tuples.py
@@ -0,0 +1,3 @@
+mi_tupla = (16, "Guillermo", False, 45.32, 16, 16, 16)
+
+print(mi_tupla.count(16))
---
+++ 8.Dictionary.py
@@ -0,0 +1,4 @@
+my_dic = {"Nom": "Carlos", "Cognom": "Macho", "Edat": 49}
+
+my_dic["Alçada"] = 1.70
+print(my_dic)
