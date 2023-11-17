b9f005948f65af6376c0babf5a4e431023894e38
Guillermo <guilleballarin73@gmail.com>
2023-11-17 08:24:54 +0100

    nn

---
+++ Probabilidad/Proyectos/urnes.py
@@ -0,0 +1,6 @@
+"""Es tenen dues urnes, i cadascuna conté un nombre diferent de boles blanques i vermelles:
+Primera urna, U1: 3 boles blanques i 2 vermelles;
+Segona urna, U2: 4 boles blanques i 2 vermelles.
+Es realitza el següent experiment aleatori:
+Es llença una moneda a l'aire i si surt cara es tria una bola de la primera urna, i si surt creu de la segona.
+Quina és la probabilitat que surti una bola blanca?"""
